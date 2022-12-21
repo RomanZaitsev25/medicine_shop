bb577bc0ead6f4c5aeed6f33595750a550615e3c
name <Romanzaitsev25@yandex.by>
2022-12-21 19:27:23 +0300

    my_project init

---
+++ medicine_shop_project/staff_side/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+from django.core.cache import cache
+from django.db import models
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Staff(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    first_name = models.CharField(max_length=250)
+    second_name = models.CharField(max_length=250)
+    position = models.CharField(max_length=250)
+    age = models.IntegerField(null=True)
+    salary = models.FloatField(default=0.0)
+    vacations = models.IntegerField(default=25)
+    slug = models.SlugField(default='', null=False)
+
+    def __str__(self):
+        return f"{self.first_name} - {self.second_name} - {self.position=}" \
+               f" - {self.age=} - {self.salary=}$" \
+               f"-{self.vacations}"
