9f64756a9e2bc19c49b78e8be7acb40b10503ec8
name <Romanzaitsev25@yandex.by>
2022-12-22 23:06:05 +0300

    first changes

--- medicine_shop_project/staff_side/models.py
+++ medicine_shop_project/staff_side/models.py
@@ -11,16 +11,38 @@ from django.urls import reverse
 
 
 class Staff(models.Model):
+    SCHEDULE_TYPE = [('FLEXIBLE', 'Flexible'), ('FULL_TIME', 'Full time')]
     id = models.AutoField(primary_key=True, unique=True)
     first_name = models.CharField(max_length=250)
     second_name = models.CharField(max_length=250)
-    position = models.CharField(max_length=250)
-    age = models.IntegerField(null=True)
-    salary = models.FloatField(default=0.0)
+    age = models.IntegerField(null=False)
+    schedule_type = models.CharField(max_length=250, choices=SCHEDULE_TYPE, default='FULL_TIME')
+    background = models.CharField(max_length=250)
+    position = models.ForeignKey('Positions', on_delete=models.PROTECT)
+    _salary = models.FloatField(default=0.0)
     vacations = models.IntegerField(default=25)
-    slug = models.SlugField(default='', null=False)
+
 
     def __str__(self):
-        return f"{self.first_name} - {self.second_name} - {self.position=}" \
-               f" - {self.age=} - {self.salary=}$" \
-               f"-{self.vacations}"
+        return f"Имя: {self.first_name} Фамилия: {self.second_name} Должность: {self.position}" \
+               f"Возраст: {self.age} Зарплата: {self._salary}$" \
+               f"Количество дней отпуска: {self.vacations}"
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, value):
+        self._salary = value
+        self.save()
+
+
+class Positions(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    post_name = models.CharField(max_length=250)
+    responsibility = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f" Позиция: {self.post_name} Обязанности: {self.responsibility}"
+#
