3f96e0ee391e800e6b416a907db0882a51c23d6b
name <Romanzaitsev25@yandex.by>
2022-12-28 09:50:52 +0300

    new models, change in apps

--- medicine_shop_project/staff_side/apps.py
+++ medicine_shop_project/staff_side/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StaffSideConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staff_side'
+    verbose_name = 'сотрудники аптеки'
